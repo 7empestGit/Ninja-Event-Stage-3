822c7867bc65814371092713ed8e3fffde14ba2b
Ismayil Mammadov <ismayilmammadov10@gmail.com>
2021-12-08 01:16:40 +0400

    Particle System

--- Assets/App/Scripts/Car Controllers/CarController.cs
+++ Assets/App/Scripts/Car Controllers/CarController.cs
@@ -38,8 +38,30 @@ public class CarController : MonoBehaviour
 
     public void FixedUpdate()
     {
-        float motor = maxMotorTorque; //* Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        #region Screen Touch
+        float touchSide = 0;
+        if (Input.touchCount == 1)
+        {
+            var touch = Input.touches[0];
+            // left side of the screen
+            if (touch.position.x < Screen.width / 2)
+            {
+                touchSide = 1;
+            }
+            // right side of the screen
+            else if (touch.position.x > Screen.width / 2)
+            {
+                touchSide = -1;
+            }
+        }
+        else
+        {
+            touchSide = 0;
+        }
+        #endregion
+
+        float motor = maxMotorTorque;   //* Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * touchSide;  //* Input.GetAxis("Horizontal");
 
         foreach (AxleInfo axleInfo in axleInfos)
         {
