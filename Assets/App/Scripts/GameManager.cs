4309c27674f73f1096db76f3cf93b18062b23ef3
7empestGit <ismayilmammadov10@gmail.com>
2022-01-21 22:21:05 +0400

    Minor changes

--- Assets/App/Scripts/GameManager.cs
+++ Assets/App/Scripts/GameManager.cs
@@ -3,7 +3,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameDataSO gameData;
-    
+    [SerializeField] private Cinemachine.CinemachineVirtualCamera mainCamera;
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private GameObject[] carsPrefab;
+
+
+    private int collectedCoins;
+    public int CollectedCoins
+    {
+        set { collectedCoins = value; }
+        get { return collectedCoins; }
+    }
+
     #region Singleton
     public static GameManager instance;
     void Awake()
@@ -12,11 +23,13 @@ public class GameManager : MonoBehaviour
     }
     #endregion
 
-    private int collectedCoins;
-    public int CollectedCoins
+    void Start()
     {
-        set { collectedCoins = value; }
-        get { return collectedCoins; }
+        // Instantiating selected car
+        GameObject carInstance = Instantiate(carsPrefab[gameData.SelectedCarID], startPoint.position, Quaternion.identity);
+        // Setting camera
+        mainCamera.Follow = carInstance.transform;
+        mainCamera.LookAt = carInstance.transform;
     }
 
     public void UpdateCollectedCoins()
