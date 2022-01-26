75fb4a745a9865181a5db9e1e4290d54165b91c0
7empestGit <ismayilmammadov10@gmail.com>
2022-01-26 04:02:58 +0400

    SaveSystem Update

--- Assets/App/Scripts/Save Management/SaveSystem.cs
+++ Assets/App/Scripts/Save Management/SaveSystem.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class SaveSystem : MonoBehaviour
 {
-    void Awake()
+    private GameData gameData;
+    void OnEnable()
     {
-        // AutoLoad data on the scene begins
-        GameDataHandler.LoadState();
+        // AutoLoad data on object enables
+       gameData = GameDataHandler.LoadState();
+    }
+
+    void OnDisable()
+    {
+        // AutoSave data on object disables
+        GameDataHandler.SaveState(gameData);
     }
 }
