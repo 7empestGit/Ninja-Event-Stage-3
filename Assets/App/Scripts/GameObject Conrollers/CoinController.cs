803f62f507b8b40d16b241e63c5a9c3d1a8a08dd
Ismayil Mammadov <ismayilmammadov10@gmail.com>
2022-02-01 03:05:27 +0400

    Audio/SFX Update

--- Assets/App/Scripts/GameObject Conrollers/CoinController.cs
+++ Assets/App/Scripts/GameObject Conrollers/CoinController.cs
@@ -4,11 +4,13 @@ public class CoinController : MonoBehaviour
 {
     [SerializeField] private GameObject coinObject;
     [SerializeField] private ParticleSystem coinParticleSystem;
+    [SerializeField] private AudioSource coinSound;
 
     void OnTriggerEnter(Collider other)
     {
         GameManager.instance.CollectedCoins++;
         PlayStateUIManager.instance.UICoinUpdate();
+        coinSound.Play();
         coinParticleSystem.Play();
         Destroy(coinObject);
         Destroy(gameObject, 1f);
